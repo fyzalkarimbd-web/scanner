4c93e8eccfbaea8ba35b08261201a44b68ba6648
fyzalkarimbd-web <fyzalkarimbd@gmail.com>
2026-05-29 21:09:18 +0600

    ala-aipp.js

--- ala-aipp.js
+++ ala-aipp.js
@@ -122,12 +122,12 @@ window.hideAiDemo = function(num) {
 
 
 (function() {
-    var authorizedDomain = "www.idcardscannerpro.com"; // আপনার নিজের ডোমেইন
+    var authorizedDomain = "www.alasofto.com"; // আপনার নিজের ডোমেইন
     var currentDomain = window.location.hostname;
 
     // যদি বর্তমান ডোমেইন আপনার ডোমেইনের সাথে না মিলে
-    if (currentDomain !== authorizedDomain && currentDomain !== "idcardscannerpro.com") {
-        alert("Warning: This is a stolen copy of ID Card Scanner Pro! Redirecting to original site...");
+    if (currentDomain !== authorizedDomain && currentDomain !== "alasofto.com") {
+        alert("Warning: This is a stolen copy of ALA Soft! Redirecting to original site...");
         window.location.href = "https://" + authorizedDomain + "/";
     }
 })();
